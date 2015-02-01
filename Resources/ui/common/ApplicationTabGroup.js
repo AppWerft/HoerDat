60fa6e513bd831f581198c2d043ac6c41fc55915
Rainer Schleevoigt <fuerst@Rainers-MBP.fritz.box>
2015-02-01 17:17:35 +0100

    Init

---
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -0,0 +1,31 @@
+function ApplicationTabGroup(Window) {
+    //create module instance
+    var self = Ti.UI.createTabGroup({
+        fullscreen : true
+    });
+
+    //create app tabs
+    var win1 = new Window('Heute, morgen …'),
+        win2 = new Window('Datenbanksuche');
+
+    var tab1 = Ti.UI.createTab({
+        title : 'Was läuft?',
+       
+        window : win1
+    });
+    win1.containingTab = tab1;
+
+    var tab2 = Ti.UI.createTab({
+        title :'Datenbanksuche',
+        
+        window : win2
+    });
+    win2.containingTab = tab2;
+
+    self.addTab(tab1);
+    self.addTab(tab2);
+
+    return self;
+};
+
+module.exports = ApplicationTabGroup;
---
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -0,0 +1,9 @@
+function ApplicationWindow(title) {
+	var self = Ti.UI.createWindow({
+		title:title,
+		backgroundColor:'white'
+	});
+	return self;
+};
+
+module.exports = ApplicationWindow;

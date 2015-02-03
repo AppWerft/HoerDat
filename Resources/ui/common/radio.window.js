7099e1fc100636d5a267503b370d7afd5d55b2d8
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2015-02-03 18:24:16 +0100

           

--- Resources/ui/common/radio.window.js
+++ Resources/ui/common/radio.window.js
@@ -2,9 +2,6 @@ var TiCircularSlider = require('de.marcelpociot.circularslider');
 
 module.exports = function() {
     var window = Ti.UI.createWindow({
-        title : title,
-        width:Ti.UI.FILL,
-        height:Ti.UI.FILL,
         backgroundColor : 'white'
     });
     var sliderView = TiCircularSlider.createView({
@@ -22,15 +19,20 @@ module.exports = function() {
     var stations = ['dlr', 'sr', 'wdr', 'ndr', 'mdr', 'br', 'dlf', 'rbb', 'rb'];
     var stationviews = [];
     for (var i = 0; i < stations.length; i++) {
-        stationviews[i] = Ti.UI.createView({
+        stationviews[i] = Ti.UI.createImageView({
             image : '/images/' + stations[i] + '.png',
-            width : 100,
-            height : 100,
+            width : 200,
+            bottom : 100,
+            height : 200,
             transform : Ti.UI.create2DMatrix({
-                rotate : 360 / stations.length * i
+                rotate : 360 / stations.length * i,
+                anchorPoint : {
+                    x : 0.9,
+                    y : 2
+                }
             })
         });
         window.add(stationviews[i]);
     }
-
+    return window;
 };

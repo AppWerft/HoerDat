6564e718eb74beed5fab0dec6a6200404b4b77ef
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-04-04 16:01:57 +0200

          

--- Resources/app.js
+++ Resources/app.js
@@ -1,10 +1,13 @@
 (function() {
-	var ChromeCast = require('ti.chromecast');
-	var res = ChromeCast.startMediaRouter('543FE7A7',function(_e) {
-		console.log(_e);
+	
+	/* starting Chromecast */
+	var deviceManager = require('ti.chromecast').createDeviceManager();
+	deviceManager.getMediaRouteSelector('DEFAULT_MEDIA_RECEIVER');
+	deviceManager.addEventListener('deviceOnline', function(e) {
+		var device = e.device;
 	});
+	
 	var Window = require('ui/handheld/ApplicationWindow');
 	var ApplicationTabGroup = require('ui/common/ApplicationTabGroup');
 	new ApplicationTabGroup(Window).open();
-
 })();

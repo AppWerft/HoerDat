0c17c11b7656ad4bc4cd9aa7e395a08f5f3b8dea
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-04-26 11:39:28 +0200

          

--- Resources/ui/common/radiowheel.onopen.js
+++ Resources/ui/common/radiowheel.onopen.js
@@ -1,23 +1,24 @@
 var Visualizer = require('ti.audiovisualizerview');
 
 module.exports = function(_e) {
+	console.log('Info: apiName=' + _e.source.apiName);
 	require('vendor/permissions').requestPermissions(['RECORD_AUDIO', 'MODIFY_AUDIO_SETTINGS'], function(_success) {
 		if (_success == true) {
-			var $ = _e.source;
+			var window = _e.source;
 			$.visualizerView = Visualizer.createView({
 				height : 90,
 				top : 30,
 				left : 20,
-				lifecycleContainer : $,
+				lifecycleContainer : window,
 				right : 20,
 				touchEnabled : false,
 				audioSessionId : 0, //playStopControlView.audioSessionId || 0,
 				width : Ti.UI.FILL,
 			});
-			$.visualizerView.addEventListener('ready', function() {
-				$.visualizerView.addBarGraphRenderers();
+			window.visualizerView.addEventListener('ready', function() {
+				window.visualizerView.addBarGraphRenderers();
 			});
-			$.add($.visualizerView);
+			window.add($.visualizerView);
 		} else
 			console.log('Pheelicks: WRONG PERMISSIONS');
 	});
